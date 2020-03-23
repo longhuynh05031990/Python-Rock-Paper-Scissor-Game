64a497f6cb1f91403ecc2b7f67c34a3c57fed6f9
longhuynh05031990 <long.huynh05031990@gmail.com>
2020-03-23 17:44:51 +0700

    2nd commit: Added GameplayHandler module

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -2,9 +2,13 @@
 #                   Import
 #############################################
 import tkinter as Tk
+import tkinter.simpledialog
 import os
+import sys
 import random
 from PIL import Image, ImageTk
+from functions import GamePlayHandler as Gp
+
 #############################################
 #                   Macros
 #############################################
@@ -13,29 +17,38 @@ ICON = '.\\images\\icon.ico'
 ROCK_ICON = ''
 PAPER_ICON = ''
 SCISSOR_ICON = ''
+NONE_ICON = ''
 for r, d, f in os.walk(IMAGE_DIR, topdown=True):
     for name in f:
         if 'Rock' in name:
             ROCK_ICON = os.path.join(r, name)
         elif 'Paper' in name:
             PAPER_ICON = os.path.join(r, name)
-        else:
+        elif 'Scissor' in name:
             SCISSOR_ICON = os.path.join(r, name)
-print('Printing...' + ROCK_ICON + ' ' + PAPER_ICON + ' ' + SCISSOR_ICON)
+        else:
+            NONE_ICON = os.path.join(r, name)
+print('Printing...' + ROCK_ICON + ' ' + PAPER_ICON + ' ' + SCISSOR_ICON + ' ' + NONE_ICON)
 ##############################################
 #             Global Variables
 ##############################################
 opponent_name_list = ['Liam', 'Noah', 'William', 'James', 'Oliver',
                       'Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia']
-
+global_player_name = ''
 ##############################################
 #                     UI
 ##############################################
+
 main_window = Tk.Tk()
 main_window.geometry('384x320')
 main_window.title("Rock Paper Scissor Game")
 main_window.iconbitmap(default=ICON)
 
+# Display a prompt for the player to input their name
+while global_player_name == '':
+    global_player_name = Tk.simpledialog.askstring('Name', 'What is your name?')
+
+main_window.lift()
 # Display a toolbar with File and Leaderboard submenus
 menu_bar = Tk.Menu(main_window)
 main_window.config(menu=menu_bar)
@@ -45,15 +58,26 @@ fileMenu = Tk.Menu(menu_bar)
 fileMenu.add_command(label="New")
 fileMenu.add_command(label="Save")
 fileMenu.add_command(label="Load")
+fileMenu.add_command(label="Exit", command=sys.exit)
+
+# Option submenu
+optionMenu = Tk.Menu(menu_bar)
+optionMenu.add_command(label="Check Number of Games You Won", command=Gp.check_game_won)
 
 # Leaderboard submenu
 lbMenu = Tk.Menu(menu_bar)
 lbMenu.add_command(label='View Leaderboard')
 lbMenu.add_command(label='Clear Leaderboard')
 
+# About submenu
+aboutMenu = Tk.Menu(menu_bar)
+aboutMenu.add_command(label="About the Author")
+
 # Display the toolbar
 menu_bar.add_cascade(label="File", menu=fileMenu)
+menu_bar.add_cascade(label="Option", menu=optionMenu)
 menu_bar.add_cascade(label="Leaderboard", menu=lbMenu)
+menu_bar.add_cascade(label="About", menu=aboutMenu)
 
 # Display the name of the opponent
 opponent_name = opponent_name_list[random.randrange(0, 9)]
@@ -65,50 +89,76 @@ opponent_name_canvas.grid(row=0, column=0)
 
 # Display the current score of the opponent
 opponent_score_canvas = Tk.Canvas(main_window, width=128, height=128, confine=True)
-opponent_score_text = Tk.Label(opponent_score_canvas, text='1', height=1, width=5, pady=32)
+opponent_score_text = Tk.Label(opponent_score_canvas, text=str(Gp.opponent_score), height=1, width=5, pady=32)
 opponent_score_text.config(font=("Courier", 30))
 opponent_score_text.pack()
 opponent_score_canvas.grid(row=0, column=2)
 
+# Display the current hand of the opponent
+opponent_hand = ImageTk.PhotoImage(file=NONE_ICON)
+opponent_hand_canvas = Tk.Canvas(main_window, width=128, height=128, bg='Red', confine=True)
+opponent_hand_canvas.create_image(64, 64, image=opponent_hand)
+opponent_hand_canvas.grid(row=0, column=1)
+
 # Display the name of the player
-player_name = opponent_name_list[random.randrange(0, 9)]
 player_name_canvas = Tk.Canvas(main_window, width=128, height=128, confine=True)
-player_name_text = Tk.Label(player_name_canvas, text=player_name, height=1, width=10, pady=32)
+player_name_text = Tk.Label(player_name_canvas, text=global_player_name, height=1, width=10, pady=32)
 player_name_text.config(font=("Courier", 15))
 player_name_text.pack()
 player_name_canvas.grid(row=1, column=2)
 
 # Display the current score of the player
 player_score_canvas = Tk.Canvas(main_window, width=128, height=128, confine=True)
-player_score_text = Tk.Label(player_score_canvas, text='1', height=1, width=5, pady=32)
+player_score_text = Tk.Label(player_score_canvas, text=str(Gp.player_score), height=1, width=5, pady=32)
 player_score_text.config(font=("Courier", 30))
 player_score_text.pack()
 player_score_canvas.grid(row=1, column=0)
 
-# Display the current hand of the opponent
-opponent_hand = ImageTk.PhotoImage(file=ROCK_ICON)
-opponent_hand_canvas = Tk.Canvas(main_window, width=128, height=128, bg='Red', confine=True)
-opponent_hand_canvas.create_image(64, 64, image=opponent_hand)
-opponent_hand_canvas.grid(row=0, column=1)
-
 # Display the current hand of the player
-player_hand = ImageTk.PhotoImage(file=SCISSOR_ICON)
+player_hand = ImageTk.PhotoImage(file=NONE_ICON)
 player_hand_canvas = Tk.Canvas(main_window, width=128, height=128, bg='Blue', confine=True)
 player_hand_canvas.create_image(64, 64, image=player_hand)
 player_hand_canvas.grid(row=1, column=1)
 
-# Display the name of the player
+##############################################
+#                 Functions
+##############################################
+# on_play_wrapper
+# Description: Function to display the image of player's choice
+# Parameters: choice - input from player by pressing the buttons
+# Return: void
+# Note: To be used as command for each button on the UI
+
+
+def on_play_wrapper(choice):
+    global opponent_score_text
+    global player_score_text
+    image_dict = {
+        0: ROCK_ICON,
+        1: PAPER_ICON,
+        2: SCISSOR_ICON
+    }
+    player_hand_tmp_img = ImageTk.PhotoImage(file=image_dict.get(choice, 'Invalid Choice'))
+    player_hand_canvas.create_image(64, 64, image=player_hand_tmp_img)
+    opponent_hand_tmp = Gp.on_play(choice)
+    opponent_score_text.config(text=str(Gp.opponent_score))
+    opponent_hand_tmp_img = ImageTk.PhotoImage(file=image_dict.get(opponent_hand_tmp, 'Invalid Choice'))
+    opponent_hand_canvas.create_image(64, 64, image=opponent_hand_tmp_img)
+    player_score_text.config(text=str(Gp.player_score))
+    Gp.display_winner()
 
 # toolbar = Tk.Frame(main_window)
 
 # All buttons to play Rock Paper Scissor
-b = Tk.Button(main_window, text='Rock', width=10)
+
+
+b = Tk.Button(main_window, text='Rock', width=10, command=lambda: on_play_wrapper(0))
 b.grid(row=3, column=0, pady=15)
 
-b = Tk.Button(main_window, text='Paper', width=10)
+b = Tk.Button(main_window, text='Paper', width=10, command=lambda: on_play_wrapper(1))
 b.grid(row=3, column=1)
 
-b = Tk.Button(main_window, text='Scissor', width=10)
+b = Tk.Button(main_window, text='Scissor', width=10, command=lambda: on_play_wrapper(2))
 b.grid(row=3, column=2)
 
 # toolbar.grid(row=3, column=1)
---
+++ functions/GamePlayHandler.py
@@ -0,0 +1,147 @@
+#############################################
+#                   Import
+#############################################
+import random
+import sys
+from tkinter import messagebox
+
+#############################################
+#                   Macros
+#############################################
+
+##############################################
+#             Global Variables
+##############################################
+session = None
+start_flag = False
+win_flag = False
+lose_flag = False
+draw_flag = False
+match_won_flag = False
+match_lost_flag = False
+opponent_score = 0
+player_score = 0
+game_won = 0
+##############################################
+#                  Classes
+##############################################
+
+
+class GamePlayHandler:
+    # Class Variables
+
+    # Class Constructor
+    def __init__(self):
+        self.opponent_hand = 0
+        self.player_hand = 0
+
+    # Class Functions
+    def get_opponent_hand(self):
+        self.opponent_hand = random.randrange(0, 2)
+        return self.opponent_hand
+
+    def set_player_hand(self, input):
+        global start_flag
+        start_flag = True
+        self.player_hand = input
+
+    def check_winner(self):
+        global lose_flag
+        global win_flag
+        global draw_flag
+        global opponent_score
+        global player_score
+        global match_won_flag
+        global match_lost_flag
+        global game_won
+
+        result = self.player_hand - self.opponent_hand
+        if start_flag:
+            if (result == -1) or (result == 2):
+                lose_flag = True
+                opponent_score += 1
+            elif (result == 1) or (result == -2):
+                win_flag = True
+                player_score += 1
+            else:
+                draw_flag = True
+                # Do nothing
+        if player_score == 3:
+            match_won_flag = True
+            game_won += 1
+            GamePlayHandler.reset()
+        if opponent_score == 3:
+            match_lost_flag = True
+            GamePlayHandler.reset()
+    @staticmethod
+    def reset():
+        global opponent_score
+        global player_score
+        global game_won
+        opponent_score = 0
+        player_score = 0
+        game_won = 0
+
+##############################################
+#                  Functions
+##############################################
+
+
+def on_new():
+    global session
+    session = GamePlayHandler()
+    # session.reset()
+
+
+def on_opponent_choice():
+    global session
+    return session.get_opponent_hand()
+
+
+def on_play(num):
+    global session
+    on_new()
+    session.set_player_hand(num)
+    opponent_hand_tmp = on_opponent_choice()
+    session.check_winner()
+    return opponent_hand_tmp
+
+
+def display_winner():
+    global lose_flag
+    global win_flag
+    global draw_flag
+    global match_won_flag
+    global match_lost_flag
+    if lose_flag:
+        messagebox.showinfo(title='You Lose', message='Sorry, The Opponent Won The Round!')
+        lose_flag = False
+    elif win_flag:
+        messagebox.showinfo(title='You Win', message='Congratulation, You Won The Round!')
+        win_flag = False
+    elif draw_flag:
+        messagebox.showinfo(title='Draw', message='Welp, Looks Like That\'s A Draw!')
+        draw_flag = False
+    else:
+        messagebox.showinfo(title='???', message='Something Went Wrong!')
+        sys.exit()
+
+    if match_won_flag:
+        messagebox.showinfo(title='Match Won', message='You Won The Match!')
+        match_won_flag = False
+    if match_lost_flag:
+        messagebox.showinfo(title='Match Lost', message='Sorry, You Lost The Match!')
+        match_lost_flag = False
+
+
+def check_game_won():
+    global game_won
+    messagebox.showinfo(title='Number of Games Won', message='You Have Won ' + str(game_won) + ' Games.')
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+   main()
